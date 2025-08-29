a23e3d5d12abc46cc7f6dc753e9521da9a1f0fd2
wanessa <wanessaldantas@gmail.com>
2025-08-29 14:44:26 -0300

    style(formulario): implementa divisão de tela em colunas 50%/50%

---
+++ src/components/Cols.tsx
@@ -0,0 +1,12 @@
+export default function Cols() {
+  return (
+    <>
+      <div className="bg-blue-500 flex items-center justify-center">
+        <p className="text-white text-xl">Formulário</p>
+      </div>
+      <div className="bg-green-500 flex items-center justify-center">
+        <p className="text-white text-xl">Preview</p>
+      </div>
+    </>
+  );
+}
