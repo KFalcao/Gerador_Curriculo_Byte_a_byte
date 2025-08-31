e6373ec1de394d3c2c99bce47a8bc7eba2ab4a3c
wanessa <wanessaldantas@gmail.com>
2025-08-31 15:55:02 -0300

    fix(formulario): ajustes no componente Cols.tsx

--- src/components/Cols.tsx
+++ src/components/Cols.tsx
@@ -1,10 +1,10 @@
 export default function Cols() {
   return (
     <>
-      <div className="bg-blue-500 flex items-center justify-center">
-        <p className="text-white text-xl">Formulário</p>
+      <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+        <p className="text-white text-xl">Informações Pessoais</p>
       </div>
-      <div className="bg-green-500 flex items-center justify-center">
+      <div className="bg-green-500 min-h-screen flex items-center justify-center">
         <p className="text-white text-xl">Preview</p>
       </div>
     </>
